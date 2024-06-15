02efc2e6aa958ca43120d0cac1c75d8c20066b04
Tomás Altrui <altruitomas@gmail.com>
2024-06-15 18:38:54 -0300

    revert stuff

--- router.js
+++ router.js
@@ -1,15 +1,13 @@
-import $ from "jquery";
-
 const pageMaps = {
-  home: "src/Home/index.html",
-  game1: "src/Games/Game1/index.html",
-  game2: "src/Games/Game2/index.html",
-  game3: "src/Games/Game3/index.html",
-  game4: "src/Games/Game4/index.html",
-  game5: "src/Games/Game5/index.html",
-  group: "src/Group/index.html",
-  games: "src/Games/index.html",
-  progress: 'src/Progress/index.html',
+  home: "home/index.html",
+  game1: "games/game1/index.html",
+  game2: "games/game2/index.html",
+  game3: "games/game3/index.html",
+  game4: "games/game4/index.html",
+  game5: "games/game5/index.html",
+  group: "group/index.html",
+  games: "games/index.html",
+  progress: 'progress/index.html',
 };
 
 $(function () {
